03086c1b0d5e72bc0fe18dd803ac7e9676f539d8
Eric <eric@rigsb.net>
2020-12-03 20:01:52 -0500

    Updating anchorbot.py to grab screenshots instead of text

--- app/anchorbot.py
+++ app/anchorbot.py
@@ -6,6 +6,7 @@ import io
 import os
 import time
 import discord
+from discord.ext import commands
 
 class AnchorBot:
 
@@ -17,16 +18,16 @@ class AnchorBot:
         password = os.environ['PASSWORD']
         self.username = username
         self.password = password
-        self.bot = webdriver.Firefox(options=options)        
+        self.browser = webdriver.Firefox(options=options)        
         
     def login(self):
         # Login to anchor.fm
-        bot = self.bot
-        bot.get('https://anchor.fm/login')
+        browser = self.browser
+        browser.get('https://anchor.fm/login')
         time.sleep(5)
         
-        email = bot.find_element_by_name('email')
-        password = bot.find_element_by_name('password')
+        email = browser.find_element_by_name('email')
+        password = browser.find_element_by_name('password')
         
         email.clear()
         password.clear()
@@ -38,47 +39,36 @@ class AnchorBot:
     def genimg(self):
         anchorbot.login()
         time.sleep(12)
-        bot = self.bot
+        browser = self.browser
 
         snapshot_image_path = 'snapshot_screenshot.png'
-        snapshot_image = bot.find_element_by_xpath("//div[@class = 'css-av84af']").screenshot_as_png
+        snapshot_image = browser.find_element_by_xpath("//div[@class = 'css-av84af']").screenshot_as_png
         snapshot_imageStream = io.BytesIO(snapshot_image)
         snapshot_im = Image.open(snapshot_imageStream)
         snapshot_im.save(snapshot_image_path)
 
         weekly_image_path = 'weekly_screenshot.png'
-        weekly_image = bot.find_element_by_xpath("//div[@class = 'VictoryContainer']").screenshot_as_png
+        weekly_image = browser.find_element_by_xpath("//div[@class = 'VictoryContainer']").screenshot_as_png
         weekly_imageStream = io.BytesIO(weekly_image)
         weekly_im = Image.open(weekly_imageStream)
         weekly_im.save(weekly_image_path)
         
     def exec(self):
         token = os.environ['TOKEN']
-        client = discord.Client()
+        bot = commands.Bot(command_prefix='!')
+        @bot.command(name='stats')
+        async def anchor_stats(ctx):
+            anchorbot.genimg()
+            snapshot_image_path = 'snapshot_screenshot.png'
+            weekly_image_path = 'weekly_screenshot.png'
+            snapshot_stats = 'Here are the current Beers in The Lot Podcast stats from anchor.fm.'
+            weekly_stats = 'Here is the total plays per week trend.'
+            await ctx.send(snapshot_stats)
+            await ctx.channel.send(file=discord.File(snapshot_image_path))
+            await ctx.channel.send(weekly_stats)
+            await ctx.channel.send(file=discord.File(weekly_image_path))
 
-        @client.event
-        async def on_ready():
-            print(f'{client.user} has connected to Discord!')
-
-        @client.event
-        async def on_message(message):
-            if message.author == client.user:
-                return
-                
-            if message.content == 'stats!':
-                anchorbot.genimg()
-                time.sleep(6)
-                bot = self.bot
-                snapshot_image_path = 'snapshot_screenshot.png'
-                weekly_image_path = 'weekly_screenshot.png'
-                snapshot_stats = 'Here are the current Beers in The Lot Podcast stats from anchor.fm.'
-                weekly_stats = 'Here is the total plays per week trend.'
-                await message.channel.send(snapshot_stats)
-                await message.channel.send(file=discord.File(snapshot_image_path))
-                await message.channel.send(weekly_stats)
-                await message.channel.send(file=discord.File(weekly_image_path))
-
-        client.run(token)
+        bot.run(token)
 
 anchorbot = AnchorBot()
 
